b73da2c594b1f3c5187bf0825cc520143f8946dd
fPetitot <petito28fl@lycee-ndduroc.com>
2018-05-22 11:06:17 +0200

    Accès BDD - Heppner

--- Cas Heppner-v0/Core/Config.php
+++ Cas Heppner-v0/Core/Config.php
@@ -6,8 +6,8 @@ class Config{
     // For database settings
     const BDD_HOST      = 'localhost';
     const BDD_NAME      = 'pro_heppner';
-    const BDD_USER      = 'pro_heppner';
-    const BDD_PASSWORD  = 'gFStNxCHAPMqeD6b';
+    const BDD_USER      = 'usersio';
+    const BDD_PASSWORD  = 'pwsio';
 
     // For Application settings
     const APPLI_NAME    = 'Heppner';
