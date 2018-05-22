27f5ef7b6104080d25073fd2c77a1d59e6e754ff
fPetitot <petito28fl@lycee-ndduroc.com>
2018-05-22 18:25:02 +0200

    Heppner - v1 (base) Florian

---
+++ Heppner - v1/Core/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core;
+
+class Config{
+    // For database settings
+    const BDD_HOST      = 'localhost';
+    const BDD_NAME      = 'pro_heppner';
+    const BDD_USER      = 'root';
+    const BDD_PASSWORD  = 'pwsio';
+
+    // For Application settings
+    const APPLI_NAME    = 'Heppner';
+    const APPLI_VERSION = 'v0';
+}
