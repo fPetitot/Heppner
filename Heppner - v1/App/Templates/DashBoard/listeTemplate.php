798410e7e291068905c64062b232904b28525b7f
fPetitot <petito28fl@lycee-ndduroc.com>
2018-05-23 17:07:51 +0200

    Presentation TDB + conducteurs sans affectation + en deplacement
    Florian

--- Heppner - v1/App/Templates/DashBoard/listeTemplate.php
+++ Heppner - v1/App/Templates/DashBoard/listeTemplate.php
@@ -34,18 +34,31 @@
         <div class="card text-white bg-primary o-hidden h-100">
             <div class="card-body">
                 <div class="card-body-icon">
-                    <i class="fa fa-fw fa-forward"></i>
+                    <i class="fa fa-fw fa-address-book"></i>
                 </div>
                 <div class="mr-5"><?php echo \App\M\ConducteurModel::count(); ?> conducteurs</div>
             </div>
         </div>
     </div>
 
+
+</div>
+
+
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="#">Test séparation</a>
+        </li>
+    </ol>
+
+
+
+<div class="row">
     <div class="col-xl-3 col-sm-6 mb-3">
         <div class="card text-white bg-primary o-hidden h-100">
             <div class="card-body">
                 <div class="card-body-icon">
-                    <i class="fa fa-fw fa-forward"></i>
+                    <i class="fa fa-fw fa-book"></i>
                 </div>
                 <div class="mr-5"><?php echo \App\M\DevisModel::devisARealiser(); ?> devis à réaliser</div>
             </div>
@@ -67,7 +80,7 @@
         <div class="card text-white bg-primary o-hidden h-100">
             <div class="card-body">
                 <div class="card-body-icon">
-                    <i class="fa fa-fw fa-forward"></i>
+                    <i class="fa fa-fw fa-building"></i>
                 </div>
                 <div class="mr-5"><?php echo \App\M\ConducteurModel::count() - \App\M\TrajetModel::getEnDeplacement(); ?> conducteurs sans affectation</div>
             </div>
