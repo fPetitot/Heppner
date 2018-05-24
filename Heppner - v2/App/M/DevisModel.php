863d5d53af42bebfc6e623cdebfca987a3916c46
thomasmot <motrieux.thomas@gmail.com>
2018-05-24 15:07:25 +0200

    chiffre d'affaire du jour

--- Heppner - v2/App/M/DevisModel.php
+++ Heppner - v2/App/M/DevisModel.php
@@ -39,14 +39,14 @@ class DevisModel extends Object
         // $listeDevis[5] : accès à la 5 eme ligne (tuple) de la liste qui est un objet de type DevisModel, mais en objet on dit qui est un élémenent de la liste qui est un objet instance de la classe DevisModel donc
         // $listeDevis[5]->getDateEnvoie() pour récupere la date d'envoie du 5 eme élément de la liste (sous entendu la ci,nqiuieme ligne de la table Devis)
         $CAJour=0;
-        $dateJour=new DateTime("NOW");
+        $dateJour=new DateTime("now",new \DateTimeZone("Europe/Paris"));
 
 
 
         // Pour chaque element de la $listeDevis, je le nomme $unDevis puis je travaille avec dans la boucle :
         foreach ($listeDevis as $unDevis){
             $dateAujourdhui = new DateTime($unDevis->getDateEnvoi());
-            if($dateJour==$dateAujourdhui){
+            if($dateJour->format('Y-m-d')==$dateAujourdhui->format('Y-m-d')){
                 $CAJour+=$unDevis->getMontant();
             }
         }
@@ -61,7 +61,7 @@ class DevisModel extends Object
         // $listeDevis[5] : accès à la 5 eme ligne (tuple) de la liste qui est un objet de type DevisModel, mais en objet on dit qui est un élémenent de la liste qui est un objet instance de la classe DevisModel donc
         // $listeDevis[5]->getDateEnvoie() pour récupere la date d'envoie du 5 eme élément de la liste (sous entendu la ci,nqiuieme ligne de la table Devis)
         $CAMois=0;
-        $dateJour=new DateTime("NOW");
+        $dateJour=new DateTime("now");
 
 
 
